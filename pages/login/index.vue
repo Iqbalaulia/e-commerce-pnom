77238b5412ee39decff39acb48767e53fe04e621
Muhammad Iqbal A.R <miqbalar12@gmail.com>
2023-11-19 22:54:13 +0700

    first commit

---
+++ pages/login/index.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="login">
+        <div class="login_header">
+            <div class="container-fluid">
+                <b-row>
+                    <b-col md="12">
+                        <div class="login_header_logo">
+                            <img src="~/assets/images/logo/logo-pnom.png" alt="logo">
+                        </div>
+                    </b-col>
+                </b-row>
+            </div>
+        </div>
+        <div class="login_content">
+            <div class="container">
+                <b-row>
+                    <b-col md="12">
+                        <div class="login_content_form">
+                            <div class="pnom-form">
+                                <b-row>
+                                    <b-col md="12" class="d-flex justify-content-center">
+                                        <div class="pnom-form_header">
+                                            <div class="title p-blue-9 text-center">masuk</div>
+                                            <div class="sub-title">
+                                                Belum punya akun <router-link to="#">Daftar</router-link>
+                                            </div>
+                                        </div>
+                                        
+                                    </b-col>
+                                    <b-col md="12">
+                                        <div class="pnom-form_content">
+                                            <div>
+                                                <label for="">Username / Email: </label>
+                                                <b-form-input placeholder="Enter your name"/>
+                                            </div>
+                                            <div class="mt-2">
+                                                <label for="">Password: </label>
+                                                <b-form-input placeholder="Enter your name"/>
+                                            </div>
+                                            <div class="forgot-password text-right mt-2 mb-2">
+                                                <router-link to="#">Lupa Password</router-link>
+                                            </div>
+
+                                              <b-button variant="primary" class="btn-pnom-primary w-100">MASUK</b-button>
+                                        </div>
+                                    </b-col>
+                                </b-row>
+                            </div>
+                        </div>
+                    </b-col>
+                </b-row>
+            </div>
+        </div>
+        <div class="login_footer">
+            <div class="container-fluid">
+                <b-row>
+                    <b-col md="12">
+                        <div class="login_footer_content">
+                            <h4 class="name">&copy; 2023, PNOMSTICKLITE</h4>
+                            <div class="devider">|</div>
+                            <h4 class="menu">Bantuan</h4>
+                        </div>
+                    </b-col>
+                </b-row>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name:'LoginPage',
+    data() {
+        return {
+            
+        }
+    },
+    components:{
+    }
+}
+</script>
---
+++ plugins/components.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import Tutorial from '~/components/layout/Tutorial.vue';
+
+Vue.component('Tutorial', Tutorial);
