3dc213efec62cd3b42a25dc5b24dd45f3f8bf6a3
Muhammad Iqbal A.R <miqbalar12@gmail.com>
2023-11-27 21:49:00 +0700

    navbar

--- components/navbar/index.vue
+++ components/navbar/index.vue
@@ -38,17 +38,32 @@
                         </b-nav-item>
                     </b-navbar-nav>
                     <b-navbar-nav>
-                        <b-nav-item href="/login">Sign In</b-nav-item>
+                        <b-input-group class="input-search" prepend="Kategori Produk">
+                            <b-form-input placeholder="Temukan Produk Disini" class="form-input"></b-form-input>
+                            <b-input-group-append>
+                              <b-button class="button-search">Cari</b-button>
+                            </b-input-group-append>
+                          </b-input-group>
                     </b-navbar-nav>
                     <b-navbar-nav>
-                        <b-nav-item href="#" class="wishlist text-center">
-                            <i class="fas fa-heart" />
-                            <div class="wishlist_title text-white">Your Wishlist</div>
-                        </b-nav-item>
-                        <b-nav-item href="#" class="cart text-center">
-                            <i class="fas fa-cart-shopping" />
-                            <div class="cart_title text-white">Your Cart</div>
-                        </b-nav-item>
+                        <div class="badge-pnom">
+                            <b-badge class="badge-wishlist" variant="danger">
+                                <span>99</span>
+                            </b-badge>
+                            <b-nav-item href="#" class="wishlist text-center">
+                                <i class="fas fa-heart" />
+                                <div class="wishlist_title text-white">Your Wishlist</div>
+                            </b-nav-item>
+                        </div>
+                        <div class="badge-pnom">
+                            <b-badge class="badge-cart" variant="danger">
+                                <span>99</span>
+                            </b-badge>
+                            <b-nav-item href="#" class="cart text-center">
+                                <i class="fas fa-cart-shopping" />
+                                <div class="cart_title text-white">Your Cart</div>
+                            </b-nav-item>
+                        </div>
                     </b-navbar-nav>
                 </b-navbar>
             </div>
