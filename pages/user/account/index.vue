e69205e66a4b52dc50964a1bb3d4a5eb6da8c535
Muhammad Iqbal A.R <miqbalar12@gmail.com>
2024-02-02 07:31:37 +0700

    fix user

---
+++ pages/user/account/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="user">
+    <navbar/>
+    <div class="user_content">
+      <div class="row">
+        <div class="col-md-3 user_content_sidebar">
+
+        </div>
+        <div class="col-md-9 border"></div>
+      </div>
+    </div>
+    <footer-ecommerce/>
+  </div>
+</template>
+<script>
+export default {
+  name:'UserAccount',
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
