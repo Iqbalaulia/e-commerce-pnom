437a40c015b1c28d696cb2ab8330bff346986263
Muhammad Iqbal A.R <miqbalar12@gmail.com>
2024-02-14 19:47:35 +0700

    feat(user/dropshipper): complete

--- store/mock/mockData.js
+++ store/mock/mockData.js
@@ -1,186 +1,255 @@
 export const mockDataMotif = [
   {
     value: "P0002",
-    name:"P0002"
+    name: "P0002",
   },
   {
     value: "P0003",
-    name:"P0003"
+    name: "P0003",
   },
   {
     value: "P0004",
-    name:"P0004"
+    name: "P0004",
   },
   {
     value: "P0005",
-    name:"P0005"
+    name: "P0005",
   },
   {
     value: "P0006",
-    name:"P00026"
-  }
-]
+    name: "P00026",
+  },
+];
 
 export const mockDataBahan = [
   {
     value: "tebal",
-    name:"tebal"
+    name: "tebal",
   },
   {
     value: "tipis",
-    name:"tipis"
+    name: "tipis",
   },
-]
+];
 
 export const mockDataRatingProductDetail = [
   {
     rating: 5,
     progress: {
-      value: 400
+      value: 400,
     },
-    total: 500
+    total: 500,
   },
   {
     rating: 4,
     progress: {
-      value: 350
+      value: 350,
     },
-    total: 200
+    total: 200,
   },
   {
     rating: 3,
     progress: {
-      value: 300
+      value: 300,
     },
-    total: 90
+    total: 90,
   },
   {
     rating: 2,
     progress: {
-      value: 200
+      value: 200,
     },
-    total: 20
+    total: 20,
   },
   {
     rating: 1,
     progress: {
-      value: 250
+      value: 250,
     },
-    total: 10
+    total: 10,
   },
   {
     rating: 0,
     progress: {
-      value: 0
+      value: 0,
     },
-    total: 0
-  }
-]
+    total: 0,
+  },
+];
 
 export const mockDataReviewersProduct = [
   {
-    imagesProfile: '',
-    name:'Uji coba',
+    imagesProfile: "",
+    name: "Uji coba",
     ratings: 5,
-    comments:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris diam ante, venenatis ac malesuada eu, fringilla in est. Duis eu feugiat nunc. In nec nibh eu metus efficitur vestibulum. Vivamus et sapien at ligula cursus suscipit eu ut augue.',
-    imagesReviews:[
+    comments:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris diam ante, venenatis ac malesuada eu, fringilla in est. Duis eu feugiat nunc. In nec nibh eu metus efficitur vestibulum. Vivamus et sapien at ligula cursus suscipit eu ut augue.",
+    imagesReviews: [
       "https://picsum.photos/1024/400/?image=41",
-      "https://picsum.photos/1024/400/?image=41"
-    ]
+      "https://picsum.photos/1024/400/?image=41",
+    ],
   },
   {
-    imagesProfile: '',
-    name:'Uji coba',
+    imagesProfile: "",
+    name: "Uji coba",
     ratings: 5,
-    comments:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris diam ante, venenatis ac malesuada eu, fringilla in est. Duis eu feugiat nunc. In nec nibh eu metus efficitur vestibulum. Vivamus et sapien at ligula cursus suscipit eu ut augue.',
-    imagesReviews:[
+    comments:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris diam ante, venenatis ac malesuada eu, fringilla in est. Duis eu feugiat nunc. In nec nibh eu metus efficitur vestibulum. Vivamus et sapien at ligula cursus suscipit eu ut augue.",
+    imagesReviews: [
       "https://picsum.photos/1024/400/?image=41",
-      "https://picsum.photos/1024/400/?image=41"
-    ]
+      "https://picsum.photos/1024/400/?image=41",
+    ],
   },
   {
-    imagesProfile: '',
-    name:'Uji coba',
+    imagesProfile: "",
+    name: "Uji coba",
     ratings: 5,
-    comments:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris diam ante, venenatis ac malesuada eu, fringilla in est. Duis eu feugiat nunc. In nec nibh eu metus efficitur vestibulum. Vivamus et sapien at ligula cursus suscipit eu ut augue.',
-    imagesReviews:[
+    comments:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris diam ante, venenatis ac malesuada eu, fringilla in est. Duis eu feugiat nunc. In nec nibh eu metus efficitur vestibulum. Vivamus et sapien at ligula cursus suscipit eu ut augue.",
+    imagesReviews: [
       "https://picsum.photos/1024/400/?image=41",
-      "https://picsum.photos/1024/400/?image=41"
-    ]
+      "https://picsum.photos/1024/400/?image=41",
+    ],
   },
   {
-    imagesProfile: '',
-    name:'Uji coba',
+    imagesProfile: "",
+    name: "Uji coba",
     ratings: 5,
-    comments:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris diam ante, venenatis ac malesuada eu, fringilla in est. Duis eu feugiat nunc. In nec nibh eu metus efficitur vestibulum. Vivamus et sapien at ligula cursus suscipit eu ut augue.',
-    imagesReviews:[
+    comments:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris diam ante, venenatis ac malesuada eu, fringilla in est. Duis eu feugiat nunc. In nec nibh eu metus efficitur vestibulum. Vivamus et sapien at ligula cursus suscipit eu ut augue.",
+    imagesReviews: [
       "https://picsum.photos/1024/400/?image=41",
-      "https://picsum.photos/1024/400/?image=41"
-    ]
+      "https://picsum.photos/1024/400/?image=41",
+    ],
   },
   {
-    imagesProfile: '',
-    name:'Uji coba',
+    imagesProfile: "",
+    name: "Uji coba",
     ratings: 5,
-    comments:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris diam ante, venenatis ac malesuada eu, fringilla in est. Duis eu feugiat nunc. In nec nibh eu metus efficitur vestibulum. Vivamus et sapien at ligula cursus suscipit eu ut augue.',
-    imagesReviews:[
+    comments:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris diam ante, venenatis ac malesuada eu, fringilla in est. Duis eu feugiat nunc. In nec nibh eu metus efficitur vestibulum. Vivamus et sapien at ligula cursus suscipit eu ut augue.",
+    imagesReviews: [
       "https://picsum.photos/1024/400/?image=41",
-      "https://picsum.photos/1024/400/?image=41"
-    ]
+      "https://picsum.photos/1024/400/?image=41",
+    ],
   },
   {
-    imagesProfile: '',
-    name:'Uji coba',
+    imagesProfile: "",
+    name: "Uji coba",
     ratings: 5,
-    comments:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris diam ante, venenatis ac malesuada eu, fringilla in est. Duis eu feugiat nunc. In nec nibh eu metus efficitur vestibulum. Vivamus et sapien at ligula cursus suscipit eu ut augue.',
-    imagesReviews:[
+    comments:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris diam ante, venenatis ac malesuada eu, fringilla in est. Duis eu feugiat nunc. In nec nibh eu metus efficitur vestibulum. Vivamus et sapien at ligula cursus suscipit eu ut augue.",
+    imagesReviews: [
       "https://picsum.photos/1024/400/?image=41",
-      "https://picsum.photos/1024/400/?image=41"
-    ]
+      "https://picsum.photos/1024/400/?image=41",
+    ],
   },
   {
-    imagesProfile: '',
-    name:'Uji coba',
+    imagesProfile: "",
+    name: "Uji coba",
     ratings: 5,
-    comments:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris diam ante, venenatis ac malesuada eu, fringilla in est. Duis eu feugiat nunc. In nec nibh eu metus efficitur vestibulum. Vivamus et sapien at ligula cursus suscipit eu ut augue.',
-    imagesReviews:[
+    comments:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris diam ante, venenatis ac malesuada eu, fringilla in est. Duis eu feugiat nunc. In nec nibh eu metus efficitur vestibulum. Vivamus et sapien at ligula cursus suscipit eu ut augue.",
+    imagesReviews: [
       "https://picsum.photos/1024/400/?image=41",
-      "https://picsum.photos/1024/400/?image=41"
-    ]
+      "https://picsum.photos/1024/400/?image=41",
+    ],
   },
   {
-    imagesProfile: '',
-    name:'Uji coba',
+    imagesProfile: "",
+    name: "Uji coba",
     ratings: 5,
-    comments:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris diam ante, venenatis ac malesuada eu, fringilla in est. Duis eu feugiat nunc. In nec nibh eu metus efficitur vestibulum. Vivamus et sapien at ligula cursus suscipit eu ut augue.',
-    imagesReviews:[
+    comments:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris diam ante, venenatis ac malesuada eu, fringilla in est. Duis eu feugiat nunc. In nec nibh eu metus efficitur vestibulum. Vivamus et sapien at ligula cursus suscipit eu ut augue.",
+    imagesReviews: [
       "https://picsum.photos/1024/400/?image=41",
-      "https://picsum.photos/1024/400/?image=41"
-    ]
-  }
-]
+      "https://picsum.photos/1024/400/?image=41",
+    ],
+  },
+];
 
 export const mockDataCart = [
-   {
-    productName: 'Striping Beat Pelindung Dashboard Tankpad Stiker Beat Deluxe 2020 2021 2022 2023 Stiker Motor Keren Variasi',
-    variant: [
-      'P001',
-      'TIPIS'
-    ],
-    price:'40500',
-    uuid: '11221212131212',
-    countProduct: '2',
-    notes:''
-   },
-   {
-    productName: 'Striping Beat Pelindung Dashboard Tankpad Stiker Beat Deluxe 2020 2021 2022 2023 Stiker Motor Keren Variasi',
-    variant: [
-      'P001',
-      'TIPIS'
-    ],
-    price:'40500',
-    uuid: '11221212131212',
-    countProduct: '1',
-    notes:''
-   }
-]
+  {
+    productName:
+      "Striping Beat Pelindung Dashboard Tankpad Stiker Beat Deluxe 2020 2021 2022 2023 Stiker Motor Keren Variasi",
+    variant: ["P001", "TIPIS"],
+    price: "40500",
+    uuid: "11221212131212",
+    countProduct: "2",
+    notes: "",
+  },
+  {
+    productName:
+      "Striping Beat Pelindung Dashboard Tankpad Stiker Beat Deluxe 2020 2021 2022 2023 Stiker Motor Keren Variasi",
+    variant: ["P001", "TIPIS"],
+    price: "40500",
+    uuid: "11221212131212",
+    countProduct: "1",
+    notes: "",
+  },
+];
+
+export const mockDataDropshipper = {
+  all_order: [
+    {
+      images:
+        "https://images.unsplash.com/photo-1583225214464-9296029427aa?q=80&w=1850&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title:
+        "Striping Beat Pelindung Dashboard Tankpad Stiker Beat Deluxe 2020 2021 2022 2023 Stiker Motor Keren Variasi",
+      status_order: "packing",
+      variant: "P002, TEBAL",
+      number_order: "PNS74676477",
+      desription:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      discount: "20.000",
+      total_after_discount: "40.000",
+      total_order: "20.000",
+    },
+    {
+      images:
+        "https://images.unsplash.com/photo-1589384267710-7a170981ca78?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title:
+        "Striping laptop all a round",
+      status_order: "packing",
+      variant: "P002, TIPIS",
+      number_order: "PNS74676477",
+      desription:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      discount: "30.000",
+      total_after_discount: "60.000",
+      total_order: "60.000",
+    },
+  ],
+  tracking_order: [
+    {
+      images:
+        "https://images.unsplash.com/photo-1621252756235-7f37e5e5125e?q=80&w=1938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title:
+        "Striping Beat Pelindung Dashboard Tankpad Stiker Beat Deluxe 2020 2021 2022 2023 Stiker Motor Keren Variasi",
+      status_order: "packing",
+      variant: "P002, TEBAL",
+      number_order: "PNS74676477",
+      desription:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      discount: "20.000",
+      total_after_discount: "40.000",
+      total_order: "20.000",
+    },
+  ],
+  bill_order: [
+    {
+      images:
+        "https://images.unsplash.com/photo-1621252756235-7f37e5e5125e?q=80&w=1938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title:
+        "Striping Beat Pelindung Dashboard Tankpad Stiker Beat Deluxe 2020 2021 2022 2023 Stiker Motor Keren Variasi",
+      status_order: "packing",
+      variant: "P002, TEBAL",
+      number_order: "PNS74676477",
+      desription:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      discount: "20.000",
+      total_after_discount: "40.000",
+      total_order: "20.000",
+    },
+  ],
+};
