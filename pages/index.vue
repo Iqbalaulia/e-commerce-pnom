686514393c7c3b342331be7dd8601fc930ea56b8
Muhammad Iqbal A.R <miqbalar12@gmail.com>
2024-02-17 15:17:18 +0700

    fix(home): slider banner

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,22 @@
   <div class="home">
     <navbar />
     <div class="home_banner">
-      <img
-        class="images-banner"
-        src="~/assets/images/banner/banner-1.png"
-        alt="banner-1"
-      />
+      <VueSlickCarousel v-bind="settings">
+        <div>
+          <img
+            class="images-banner"
+            src="~/assets/images/banner/banner-1.png"
+            alt="banner-1"
+          />
+        </div>
+        <div>
+          <img
+            class="images-banner"
+            src="~/assets/images/banner/banner.png"
+            alt="banner-1"
+          />
+        </div>
+      </VueSlickCarousel>
     </div>
     <filter-category />
     <filter-popular />
@@ -32,7 +43,17 @@
 </template>
 
 <script>
+import VueSlickCarousel from "vue-slick-carousel";
+import { carouselBanner } from "~/plugins/carousel";
+
 export default {
   name: "IndexPage",
+  data() {
+    return {
+      settings: carouselBanner,
+    };
+  },
+  components: { VueSlickCarousel },
+  methods: {},
 };
 </script>
--- plugins/carousel.js
+++ plugins/carousel.js
@@ -6,3 +6,13 @@ export const carouselProduct = {
   slidesToScroll: 5,
   touchThreshold: 5,
 };
+
+export const carouselBanner = {
+  dots: true,
+  dotsClass: "slick-dots custom-dot-class",
+  edgeFriction: 0.35,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
