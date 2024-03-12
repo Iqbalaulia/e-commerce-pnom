188a76c304ec4b40515c5b1543e8bc205f1835fe
Muhammad Iqbal A.R <miqbalar12@gmail.com>
2024-03-12 11:41:35 +0700

    build development

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,70 +1,66 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'ecommerce-pnom',
+    title: "ecommerce-pnom",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
     link: [
       {
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: '/favicon.ico'
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/favicon.ico",
       },
       {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap'
-      }
-    ]
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap",
+      },
+    ],
   },
 
   css: [
-    '~/assets/scss/main.scss',
-    'vue-slick-carousel/dist/vue-slick-carousel-theme.css',
-    'vue-slick-carousel/dist/vue-slick-carousel.css',
-    '@fortawesome/fontawesome-free/css/all.css',
+    "~/assets/scss/main.scss",
+    "vue-slick-carousel/dist/vue-slick-carousel-theme.css",
+    "vue-slick-carousel/dist/vue-slick-carousel.css",
+    "@fortawesome/fontawesome-free/css/all.css",
   ],
 
   plugins: [
-    '~/plugins/components.js',
-    '~/plugins/aos.js',
-    '~/utils/helpers.js'
+    "~/plugins/components.js",
+    "~/plugins/aos.js",
+    "~/utils/helpers.js",
   ],
 
   components: true,
 
-  buildModules: [
+  buildModules: [],
 
-  ],
-
-  modules: [
-    'bootstrap-vue/nuxt',
-    '@nuxtjs/axios'
-  ],
+  modules: ["bootstrap-vue/nuxt", "@nuxtjs/axios"],
 
   bootstrapVue: {
-    icons: true
+    icons: true,
   },
 
   build: {
+    buildDir: "build",
     loaders: {
       sass: {
-        implementation: require('sass'),
+        implementation: require("sass"),
       },
       scss: {
-        implementation: require('sass'),
-      }
-    }
+        implementation: require("sass"),
+      },
+    },
   },
   env: {
     baseURL: process.env.PNOM_BASE_URL,
     versionName: "1", //rubah versi di sini
     versionCode: 1, // dan di sini
   },
-}
+};
