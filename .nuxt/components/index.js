af40efc883782894e38bcd449835fc781436c2df
Muhammad Iqbal A.R <miqbalar12@gmail.com>
2024-03-12 12:03:18 +0700

    build development

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,21 +1,21 @@
-export { default as NuxtLogo } from '../..\\components\\NuxtLogo.vue'
-export { default as Footer } from '../..\\components\\footer\\Footer.vue'
-export { default as FooterLogin } from '../..\\components\\footer\\FooterLogin.vue'
-export { default as Header } from '../..\\components\\header\\index.vue'
-export { default as LayoutTutorial } from '../..\\components\\layout\\Tutorial.vue'
-export { default as Navbar } from '../..\\components\\navbar\\index.vue'
-export { default as ReviewCustomer } from '../..\\components\\reviewCustomer\\index.vue'
-export { default as HomeFilterArticle } from '../..\\components\\home\\filterArticle\\index.vue'
-export { default as HomeFilterCategory } from '../..\\components\\home\\filterCategory\\index.vue'
-export { default as HomeFilterNewProduct } from '../..\\components\\home\\filterNewProduct\\index.vue'
-export { default as HomeFilterPopular } from '../..\\components\\home\\filterPopular\\index.vue'
-export { default as HomeFilterRecomanded } from '../..\\components\\home\\filterRecomanded\\index.vue'
-export { default as HomeFilterRelate } from '../..\\components\\home\\filterRelate\\index.vue'
-export { default as UserAddress } from '../..\\components\\user\\address\\index.vue'
-export { default as UserDropshipper } from '../..\\components\\user\\dropshipper\\index.vue'
-export { default as UserMyAccount } from '../..\\components\\user\\myAccount\\index.vue'
-export { default as UserNotification } from '../..\\components\\user\\notification\\index.vue'
-export { default as UserOrder } from '../..\\components\\user\\order\\index.vue'
+export const NuxtLogo = () => import('../..\\components\\NuxtLogo.vue' /* webpackChunkName: "components/nuxt-logo" */).then(c => wrapFunctional(c.default || c))
+export const Footer = () => import('../..\\components\\footer\\Footer.vue' /* webpackChunkName: "components/footer" */).then(c => wrapFunctional(c.default || c))
+export const FooterLogin = () => import('../..\\components\\footer\\FooterLogin.vue' /* webpackChunkName: "components/footer-login" */).then(c => wrapFunctional(c.default || c))
+export const Header = () => import('../..\\components\\header\\index.vue' /* webpackChunkName: "components/header" */).then(c => wrapFunctional(c.default || c))
+export const LayoutTutorial = () => import('../..\\components\\layout\\Tutorial.vue' /* webpackChunkName: "components/layout-tutorial" */).then(c => wrapFunctional(c.default || c))
+export const Navbar = () => import('../..\\components\\navbar\\index.vue' /* webpackChunkName: "components/navbar" */).then(c => wrapFunctional(c.default || c))
+export const ReviewCustomer = () => import('../..\\components\\reviewCustomer\\index.vue' /* webpackChunkName: "components/review-customer" */).then(c => wrapFunctional(c.default || c))
+export const HomeFilterArticle = () => import('../..\\components\\home\\filterArticle\\index.vue' /* webpackChunkName: "components/home-filter-article" */).then(c => wrapFunctional(c.default || c))
+export const HomeFilterCategory = () => import('../..\\components\\home\\filterCategory\\index.vue' /* webpackChunkName: "components/home-filter-category" */).then(c => wrapFunctional(c.default || c))
+export const HomeFilterNewProduct = () => import('../..\\components\\home\\filterNewProduct\\index.vue' /* webpackChunkName: "components/home-filter-new-product" */).then(c => wrapFunctional(c.default || c))
+export const HomeFilterPopular = () => import('../..\\components\\home\\filterPopular\\index.vue' /* webpackChunkName: "components/home-filter-popular" */).then(c => wrapFunctional(c.default || c))
+export const HomeFilterRecomanded = () => import('../..\\components\\home\\filterRecomanded\\index.vue' /* webpackChunkName: "components/home-filter-recomanded" */).then(c => wrapFunctional(c.default || c))
+export const HomeFilterRelate = () => import('../..\\components\\home\\filterRelate\\index.vue' /* webpackChunkName: "components/home-filter-relate" */).then(c => wrapFunctional(c.default || c))
+export const UserAddress = () => import('../..\\components\\user\\address\\index.vue' /* webpackChunkName: "components/user-address" */).then(c => wrapFunctional(c.default || c))
+export const UserDropshipper = () => import('../..\\components\\user\\dropshipper\\index.vue' /* webpackChunkName: "components/user-dropshipper" */).then(c => wrapFunctional(c.default || c))
+export const UserMyAccount = () => import('../..\\components\\user\\myAccount\\index.vue' /* webpackChunkName: "components/user-my-account" */).then(c => wrapFunctional(c.default || c))
+export const UserNotification = () => import('../..\\components\\user\\notification\\index.vue' /* webpackChunkName: "components/user-notification" */).then(c => wrapFunctional(c.default || c))
+export const UserOrder = () => import('../..\\components\\user\\order\\index.vue' /* webpackChunkName: "components/user-order" */).then(c => wrapFunctional(c.default || c))
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
