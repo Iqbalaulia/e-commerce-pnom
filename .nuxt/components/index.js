6feb7d7e7476452afd56073d61ddd722d210c3a5
Muhammad Iqbal A.R <miqbalar12@gmail.com>
2024-03-12 10:25:21 +0700

    build development

---
+++ .nuxt/components/index.js
@@ -0,0 +1,49 @@
+export { default as NuxtLogo } from '../..\\components\\NuxtLogo.vue'
+export { default as Footer } from '../..\\components\\footer\\Footer.vue'
+export { default as FooterLogin } from '../..\\components\\footer\\FooterLogin.vue'
+export { default as Header } from '../..\\components\\header\\index.vue'
+export { default as LayoutTutorial } from '../..\\components\\layout\\Tutorial.vue'
+export { default as Navbar } from '../..\\components\\navbar\\index.vue'
+export { default as ReviewCustomer } from '../..\\components\\reviewCustomer\\index.vue'
+export { default as HomeFilterArticle } from '../..\\components\\home\\filterArticle\\index.vue'
+export { default as HomeFilterCategory } from '../..\\components\\home\\filterCategory\\index.vue'
+export { default as HomeFilterNewProduct } from '../..\\components\\home\\filterNewProduct\\index.vue'
+export { default as HomeFilterPopular } from '../..\\components\\home\\filterPopular\\index.vue'
+export { default as HomeFilterRecomanded } from '../..\\components\\home\\filterRecomanded\\index.vue'
+export { default as HomeFilterRelate } from '../..\\components\\home\\filterRelate\\index.vue'
+export { default as UserAddress } from '../..\\components\\user\\address\\index.vue'
+export { default as UserDropshipper } from '../..\\components\\user\\dropshipper\\index.vue'
+export { default as UserMyAccount } from '../..\\components\\user\\myAccount\\index.vue'
+export { default as UserNotification } from '../..\\components\\user\\notification\\index.vue'
+export { default as UserOrder } from '../..\\components\\user\\order\\index.vue'
+
+// nuxt/nuxt.js#8607
+function wrapFunctional(options) {
+  if (!options || !options.functional) {
+    return options
+  }
+
+  const propKeys = Array.isArray(options.props) ? options.props : Object.keys(options.props || {})
+
+  return {
+    render(h) {
+      const attrs = {}
+      const props = {}
+
+      for (const key in this.$attrs) {
+        if (propKeys.includes(key)) {
+          props[key] = this.$attrs[key]
+        } else {
+          attrs[key] = this.$attrs[key]
+        }
+      }
+
+      return h(options, {
+        on: this.$listeners,
+        attrs,
+        props,
+        scopedSlots: this.$scopedSlots,
+      }, this.$slots.default)
+    }
+  }
+}
--- components/home/filterNewProduct/index.vue
+++ components/home/filterNewProduct/index.vue
@@ -1,119 +1,72 @@
 <template>
-    <div class="pnom-filter-new-product">
-        <div class="pnom-filter-new-product_title">
-           <div class="data-title">
-                <div class="devider"></div>
-                <label for="">produk terbaru</label>
-           </div>
-           <div class="another-link">
-               <a href="#">
-                    lihat lainnya
-               </a>
-           </div>
-        </div>
-
-        <div class="pnom-filter-new-product_content">
-            <b-row>
-                <b-col md="3">
-                    <NuxtLink  to="/p/tankpad-vario-160-pelindung" class="section_content">
-                        <img class="images-new-product" src="~/assets/images/new-product/new-product-1.png" alt="category-1">
-                        <div class="section_content_title">
-                            tankpad vario 160 pelindung  dashboard motor
-                        </div>
-                        <div class="section_content_rupiah">
-                            Rp 24.000 - 99.000
-                        </div>
-                        <div class="section_content_rating">
-                            <div class="star-rating">
-                                <span
-                                    v-for="star in [1, 2, 3, 4, 5]"
-                                    :key="star"
-                                    class="star"
-                                >
-                                    ★
-                                </span>
-                            </div>
-                            <div class="total">1.2K Terjual/bulan</div>
-                        </div>
-                    </NuxtLink >
-                </b-col>
-                <b-col md="3">
-                    <NuxtLink to="/p/tankpad-vario-160-pelindung" class="section_content">
-                        <img class="images-new-product" src="~/assets/images/new-product/new-product-2.png" alt="category-1">
-                        <div class="section_content_title">
-                            tankpad pelindung dashboard motor beat esp 2017 - 2019
-                        </div>
-                        <div class="section_content_rupiah">
-                            Rp 24.000 - 99.000
-                        </div>
-                        <div class="section_content_rating">
-                            <div class="star-rating">
-                                <span
-                                    v-for="star in [1, 2, 3, 4, 5]"
-                                    :key="star"
-                                    class="star"
-                                >
-                                    ★
-                                </span>
-                            </div>
-                            <div class="total">1.2K Terjual/bulan</div>
-                        </div>
-                      </NuxtLink>
-                </b-col>
-                <b-col md="3">
-                    <NuxtLink to="/p/tankpad-vario-160-pelindung" class="section_content">
-                        <img class="images-new-product" src="~/assets/images/new-product/new-product-3.png" alt="category-1">
-                        <div class="section_content_title">
-                            tankpad pelindung dashboard motor beat deluxe 2020 - 2023 ...
-                        </div>
-                        <div class="section_content_rupiah">
-                            Rp 24.000 - 99.000
-                        </div>
-                        <div class="section_content_rating">
-                            <div class="star-rating">
-                                <span
-                                    v-for="star in [1, 2, 3, 4, 5]"
-                                    :key="star"
-                                    class="star"
-                                >
-                                    ★
-                                </span>
-                            </div>
-                            <div class="total">1.2K Terjual/bulan</div>
-                        </div>
-                      </NuxtLink>
-                </b-col>
-                <b-col md="3">
-                    <NuxtLink to="/p/tankpad-vario-160-pelindung" class="section_content">
-                        <img class="images-new-product" src="~/assets/images/new-product/new-product-4.png" alt="category-1">
-                        <div class="section_content_title">
-                            108 pcs sticker random aes-thetic anti air untuk hp/laptop/
-                        </div>
-                        <div class="section_content_rupiah">
-                            Rp 24.000 - 99.000
-                        </div>
-                        <div class="section_content_rating">
-                            <div class="star-rating">
-                                <span
-                                    v-for="star in [1, 2, 3, 4, 5]"
-                                    :key="star"
-                                    class="star"
-                                >
-                                    ★
-                                </span>
-                            </div>
-                            <div class="total">1.2K Terjual/bulan</div>
-                        </div>
-                      </NuxtLink>
-                </b-col>
-            </b-row>
-        </div>
+  <div class="pnom-filter-new-product">
+    <div class="pnom-filter-new-product_title">
+      <div class="data-title">
+        <div class="devider"></div>
+        <label for="">produk terbaru</label>
+      </div>
+      <div class="another-link">
+        <a href="#"> lihat lainnya </a>
+      </div>
     </div>
+
+    <div class="pnom-filter-new-product_content">
+      <b-row>
+        <b-col
+          md="3"
+          v-for="(itemProduct, indexProduct) in dataNewProduct.slice(0,4)"
+          :key="indexProduct + itemProduct.uuid"
+        >
+          <NuxtLink :to="`/p/${itemProduct.slug}`" class="section_content">
+            <img
+              class="images-new-product"
+              :src="itemProduct.imageCover"
+              alt="category-1"
+            />
+            <div class="section_content_title">
+              {{itemProduct.name}}
+            </div>
+            <div class="section_content_rupiah">{{itemProduct.price}}</div>
+            <div class="section_content_rating">
+              <div class="star-rating">
+                <span v-for="star in itemProduct.stars" :key="star" class="star">
+                  ★
+                </span>
+              </div>
+              <div class="total">1.2K Terjual/bulan</div>
+            </div>
+          </NuxtLink>
+        </b-col>
+      </b-row>
+    </div>
+  </div>
 </template>
 
-
 <script>
+import { crudMethods } from "~/store/helpers";
+
 export default {
-    name:'FilterNewProduct'
-}
+  name: "FilterNewProduct",
+  data() {
+    return {
+      dataNewProduct: [],
+    };
+  },
+  mounted() {
+    this.getListNewProduct();
+  },
+  methods: {
+    ...crudMethods,
+    async getListNewProduct() {
+      try {
+        let response = await this.getData({
+          url: `/v1.0/homepage/product/latest`,
+        });
+        this.dataNewProduct = response?.data?.data;
+      } catch (error) {
+        console.error(JSON.stringify(error));
+      }
+    },
+  },
+};
 </script>
